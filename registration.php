bea31504c8ea3dd38c4a1e3e916dc83be1eef3df
Oliverio Gombert <olivertar@gmail.com>
2026-04-25 18:54:13 +0200

    format and standard

--- registration.php
+++ registration.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This file is part of the Orangecat CompanyMethods package.
  *
@@ -8,6 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Orangecat_CompanyMethods',
